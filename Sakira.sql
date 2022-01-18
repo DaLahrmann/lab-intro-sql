238e366b2cea48e7a8a9f02300f6de54d448a88b
Daniel Lahrmann <da_lahrmann@gmx.de>
2022-01-18 16:37:52 +0100

    Task finished

---
+++ Sakira.sql
@@ -0,0 +1,18 @@
+-- Question 1
+use sakila;
+
+-- Question 2
+select * from actor;
+select * from film;
+select * from customer;
+
+-- Question 3
+select title from film;
+
+-- Question 4
+select name from language;
+
+-- Question 5
+select count(*) from address;
+select count(*) from staff;
+select first_name from staff;
